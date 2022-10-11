d3bc21948dfd0910d6c9724170686160d639899f
imashiqe <imashiqe@gmail.com>
2022-10-12 02:12:00 +0600

    header  footer added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import  './Footer.css' ;
+const Footer = () => {
+    return (
+        <div>
+            <h3>This is Footer</h3>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <div>
+            <h3>This is Header</h3>
+        </div>
+    );
+};
+
+export default Header;
